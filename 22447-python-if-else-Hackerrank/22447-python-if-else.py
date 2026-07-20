19c0b3b7d1bae8757ce6e881b60161e4bcf26ba0
Nusybah Mohamad <nusybamohamad@gmail.com>
2026-07-21 02:40:41 +0300

    if else - Time: O(1), Space: O(1)

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n % 2 == 1:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
