d5bf80579c7cf82b31e3bbeff3e98289a36ebce8
Nusybah Mohamad <nusybamohamad@gmail.com>
2026-07-27 08:05:28 +0300

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 1604-validating-roman-numerals-Hackerrank/1604-validating-roman-numerals.py
@@ -0,0 +1,9 @@
+
+
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"	# Do not delete 'r'.
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
