a42c06da36b94d087896f712fb3d63af3204ddec
Nusybah Mohamad <nusybamohamad@gmail.com>
2026-08-11 16:18:04 +0300

    Force committed solution - Time: O(x * y * z), Space: O(x * y * z)

---
+++ 1572-list-comprehensions-Hackerrank/1572-list-comprehensions.py
@@ -0,0 +1,17 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+result = [[i, j, k]
+          for i in range(x + 1)
+          for j in range(y + 1)
+          for k in range(z + 1)
+          if i + j + k != n]
+
+print(result)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
